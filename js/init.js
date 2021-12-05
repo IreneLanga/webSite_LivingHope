c3dc6ae09bc4a5f14db59c4e1cb397a99809a1fd
Irene Langa <irenelanga38@gmail.com>
2021-12-05 19:41:13 +0200

    initial commit

---
+++ js/init.js
@@ -0,0 +1,23 @@
+(function($){
+ // end of document ready
+  $(function(){
+	$( document ).ready(function() {
+      $(".button-collapse").sideNav();
+    });
+});
+
+
+  $(function(){
+
+  	$('#linkUm').click(function() {
+  $('#um').fadeIn();
+});
+
+$('#linkDois').click(function() {
+  $('#dois').fadeIn();
+});
+
+
+
+  });
+})(jQuery); // end of jQuery name space
---
+++ php/livingHope.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Living Hope</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- Third party plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="../css/style.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Living Hope</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Sobre</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Associação</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contacto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h1 class="text-uppercase text-white font-weight-bold">Esperança Viva!</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 font-weight-light mb-5">Estamos aqui para ajudar a quem precisa. Conte connosco!</p>
+                        <a class="btn btn-success btn-xl js-scroll-trigger" href="#about">A que fazemos</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-dark mb-3" id="about">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">Nós</h2>
+                        <hr class="divider light my-4" />
+                        <p class="text-white-50 mb-4"> Somos uma associação moçambicana de carácter humanitário e de desenvolvimento, com o número de entidade legal( NUEL) 101471853 cediada na Beira!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container">
+                <h2 class="text-center mt-0">Living Hope</h2>
+                <hr class="divider my-4" />
+                <div class="row">
+                    <div class="col-lg-4 col-md-8 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-gem text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Valores</h3>
+                            <p class="text-muted mb-0">Valorizarmos a diversidade humana por ser algo grandioso que deve ser respeitada para manter a paz e não descriminação e a dignidade humana por ser está uma qualidade que o protege
+                              contra todo tratamento degradante e de descriminação odiosa, assegurando condições matérias minimas de sobrevivência!</p>
+                        </div>
+                    </div>
+
+                  <div class="col-lg-4 col-md-8 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-globe text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Visão</h3>
+                            <p class="text-muted mb-0">É ter um mundo livre de desigualidades e injustiças sociais, onde as pessoas
+                            são tratadas de forma humana e digna , livres para exercerem os seus direitos fundamentais.  !</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-8 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Missão</h3>
+                            <p class="text-muted mb-0">Contribuir para elevação da dignidade humana através do
+                              acesso a Serviços básicos às comunidades desfavorecidas, garantindo-lhes direitos fundamentais, a inclusão e a participação em todos os processos inerentes ao seu desenvolvimento de forma sustentável.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Portfolio-->
+        <div id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="../assets/img/portfolio/fullsize/IMG-20210303-WA0020.jpg">
+                            <img class="img-fluid" src="../assets/img/portfolio/thumbnails/IMG-20210303-WA0020.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                              <div class="project-category text-white-50">Living Hope</div>
+                              <div class="project-name">Munhava Matopé</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="../assets/img/portfolio/fullsize/IMG-20210303-WA0008.jpg">
+                            <img class="img-fluid" src="../assets/img/portfolio/thumbnails/IMG-20210303-WA0008.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                              <div class="project-category text-white-50">Living Hope</div>
+                              <div class="project-name">Munhava Matopé</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="../assets/img/portfolio/fullsize/IMG-20210303-WA0015.jpg">
+                            <img class="img-fluid" src="../assets/img/portfolio/thumbnails/IMG-20210303-WA0015.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                              <div class="project-category text-white-50">Living Hope</div>
+                              <div class="project-name">Munhava Matopé</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="../assets/img/portfolio/fullsize/IMG-20210303-WA0018.jpg">
+                            <img class="img-fluid" src="../assets/img/portfolio/thumbnails/IMG-20210303-WA0018.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                              <div class="project-category text-white-50">Living Hope</div>
+                              <div class="project-name">Munhava Matopé</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="../assets/img/portfolio/fullsize/IMG-20210303-WA0014.jpg">
+                            <img class="img-fluid" src="../assets/img/portfolio/thumbnails/IMG-20210303-WA0014.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                              <div class="project-category text-white-50">Living Hope</div>
+                              <div class="project-name">Munhava Matopé</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="../assets/img/portfolio/fullsize/IMG-20210303-WA0009.jpg">
+                            <img class="img-fluid" src="../assets/img/portfolio/thumbnails/IMG-20210303-WA0009.jpg" alt="" />
+                            <div class="portfolio-box-caption p-3">
+                                <div class="project-category text-white-50">Living Hope</div>
+                                <div class="project-name">Munhava Matopé</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Call to action-->
+        <section class="page-section bg-success  text-white">
+            <div class="container text-center">
+                <h2 class="mb-4">A sua alegria é a nossa força!</h2>
+            </div>
+        </section>
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="mt-0">Fala Conosco</h2>
+                        <hr class="divider my-4" />
+                        <p class="text-muted mb-5">Entre em contacto connosco e vamos lhe ajudar e mudar o mundo.
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+                        <div>+ (258) 848-032-876</div>
+                    </div>
+                    <div class="col-lg-4 mr-auto text-center">
+                        <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                        <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                        <a class="d-block" href="mailto:irenelanga38@gmail.com">irenelanga@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="bg-dark py-5">
+            <div class="container"><div class="small text-center text-muted"> © 2021 - Living Hope- Todos Direitos Reservados &nbsp;&nbsp;&nbsp;
+                      <a class="btn btn-success btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
+                      <a class="btn btn-success btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                      <a class="btn btn-success btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                  </div>
+                </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="../js/scripts.js"></script>
+    </body>
+</html>
